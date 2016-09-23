97a2920fc0a33e7c8f3e1b051681b14c56b35b6e
U-STARDUST\Luis <luis@digitalicagroup.com>
2016-09-23 12:01:13 -0400

    Add new attribute to commands configuration to split parameters
    Custom split regular expressions in commands definitions will override
    the default regexp (one or more spaces by default)

--- lib/SlackHookFramework/CommandFactory.php
+++ lib/SlackHookFramework/CommandFactory.php
@@ -19,7 +19,7 @@ class CommandFactory {
 	 *
 	 * @var array|null
 	 */
-	protected static $classes = null;
+	protected static $commands = null;
 	
 	/**
 	 *
@@ -49,18 +49,23 @@ class CommandFactory {
 		self::$log->debug ( "CommandFactory: post received (json encoded): " . json_encode ( $post ) );
 		
 		// checking if commands definitions have been loaded
-		if (self::$classes == null || self::$help_data == null) {
+		if (self::$commands == null || self::$help_data == null) {
 			self::reloadDefinitions ( $config );
 		}
 		// TODO move strings parameter 'text' to global definition
-		if (isset ( $post ['text'] ) && self::$classes != null) {
+		if (isset ( $post ['text'] ) && self::$commands != null) {
 			self::$log->debug ( "CommandFactory: text received: " . $post ['text'] );
-			// parsing inputs by space
-			$input = preg_split ( "/[\\s]+/", $post ['text'] );
 			// the first word represents the command
-			if (in_array ( $input [0], array_keys ( self::$classes ) )) {
-				$class = self::$classes [$input [0]];
+			if (in_array ( $input [0], array_keys ( self::$commands ) )) {
+				$class = self::$commands [$input [0]] ["class"];
+				$split_regexp = "/[\\s]+/";
+				if (isset (self::$commands [$input [0]] ["split_regexp"])) {
+					$split_regexp = self::$commands [$input [0]] ["split_regexp"];
+				
+				// parsing inputs by space
+				$input = preg_split ( $split_regexp, $post ['text'] );
 				array_shift ( $input );
+				
 				$cmd = new $class ( $post, $config, $input );
 			}
 		}
@@ -96,11 +101,12 @@ class CommandFactory {
 		$json = json_decode ( preg_replace ( '/.+?({.+}).+/', '$1', utf8_encode ( file_get_contents ( $file ) ) ), true );
 		if ($json != null) {
 			if ($clean_previous) {
-				self::$classes = array ();
+				self::$commands = array ();
 				self::$help_data = array ();
 			}
 			foreach ( $json ["commands"] as $command ) {
-				self::$classes [$command ["trigger"]] = $command ["class"];
+				self::$commands [$command ["trigger"]] ["class"] = $command ["class"];
+				self::$commands [$command ["trigger"]] ["split_regexp"] = $command ["split_regexp"];
 				self::$help_data [$command ["help_title"]] = $command ["help_text"];
 			}
 			$result = true;
